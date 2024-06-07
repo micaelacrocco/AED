ca8c21a21362fa9a3548389284d8b0fb082c4646
micaelacrocco <micaela.crocco@correo.ucu.edu.uy>
2024-06-07 17:52:13 -0300

    + Apuntes

--- UT6_DiccionarioMapasHashing/PDs/pd4/pd4_frecuencia_palabras_hash/src/main/java/aed/App.java
+++ UT6_DiccionarioMapasHashing/PDs/pd4/pd4_frecuencia_palabras_hash/src/main/java/aed/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main( String[] args )
     {
-        String[] texto = ManejadorArchivosGenerico.leerArchivoXPalabras("UT6_DiccionarioMapasHashing\\PDs\\pd4\\libro.txt");
+        String[] texto = ManejadorArchivosGenerico2.leerArchivoXPalabras("UT6_DiccionarioMapasHashing\\PDs\\pd4\\libro.txt");
         ContadorPalabras.contarPalabras(texto);
     }
 }
